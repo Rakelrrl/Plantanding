4a0cc215d14f84761f966c2ee482f7cf2b1fa1d4
Rakelrrl <raquelriesco20@hotmail.es>
2023-04-10 15:21:38 +0200

    Co-authored-by: Gabriel Murillo Peña <gabrimurii@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+class Memorama {
+
+    constructor(){
+
+        this.can
+    }
+
+}
+
+/* Cuando el documento se haya cargado completamente */
+document.addEventListener("DOMContentLoaded", () => {
+
+    new Memorama(); 
+
+})
