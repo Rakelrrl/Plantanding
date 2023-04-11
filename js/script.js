5377476d4ced54046e7e0b7ec2318dcfe80a4f48
Rakelrrl <raquelriesco20@hotmail.es>
2023-04-11 17:13:20 +0200

    11-04-2023

--- js/script.js
+++ js/script.js
@@ -1,16 +1,44 @@
 
-class Memorama {
+// class Memorama {
 
-    constructor(){
+//     constructor(){
 
-        this.can
-    }
+//         this.can
+//     }
 
-}
+// }
 
 /* Cuando el documento se haya cargado completamente */
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
-    new Memorama(); 
+//     new Memorama(); 
 
-})
+// })
+
+
+
+/* CAROUSEL */
+
+let counter = 1;
+
+    setInterval(function(){
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if(counter > 3){
+        counter = 1;
+        }
+    }, 5000);
+
+
+
+/* CARRITO Y FAV */
+
+// const favoritos = document.querySelector("anadirFavoritos")
+
+// favoritos.addEventListener("click", function(){
+    
+// })
+
+
+
+
